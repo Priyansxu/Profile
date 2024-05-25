a0ebdb3ba5c2c8b6cc326971e12b5f5059af82e5
Priyanshu <140606611+Priyansxu@users.noreply.github.com>
2024-05-25 13:46:52 +0530

    Update page.tsx

--- app/page-2/page.tsx
+++ app/page-2/page.tsx
@@ -16,16 +16,16 @@ export default function Home() {
               02
             </span>
             <p className="pb-6 font-medium">
-             I like programming discord bots  creating websites.
-             Experience in Python, HTML, CSS and Javascript. i also design graphics whenever needed! BTW, I JUST CREATED AN AI BOT {" "}              
+             I like programming discord bots and creating websites.
+             Experience in Python, HTML, CSS and Javascript. i also design graphics whenever needed! <strong>I just created an advance Ai discord bot</strong> {" "}              
      <a href="https://top.gg/bot/1238461418999648277"
                 target="_blank"
                 rel="noreferrer"
-                className="pb-1 text-zinc-800 dark:text-zinc-100 underline font-medium"
+                className="pb-1 text-zinc-800 dark:text-zinc-100 underline bold font-medium"
               >
                 Vexel
               </a>{" "}
-           add it to your server!
+           <strong>add it to your server!</strong>
             </p>
 
             <div className="">
