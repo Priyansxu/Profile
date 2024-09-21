08b8baf08bd97e402fee4385bd678a54f4d331ae
Priyanshu <140606611+Priyansxu@users.noreply.github.com>
2024-09-21 08:31:55 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -12,12 +12,12 @@ export default function Home() {
         <Wrapper>
           <div className="mx-auto flex max-w-sm flex-col justify-between">
             <span
-              className={`-mt-14 inline-block text-[64px] font-bold text-black dark:text-white`}
+              className="-mt-14 inline-block text-[64px] font-bold text-black dark:text-white"
             >
               01
             </span>
             <p className="pb-6 font-medium">
-              Priyanshu Gupta, born in 2007. A passionate and curious individual with a keen interest in programming, graphics designing, psychology, and space exploration. Instagram {" "}
+              Priyanshu Gupta, born in 2007. A passionate and curious individual with a keen interest in programming, graphics designing, psychology, and space exploration. Instagram{" "}
               <a
                 href="https://instagram.com/priyansxu_gupta"
                 target="_blank"
@@ -28,17 +28,12 @@ export default function Home() {
               </a>{" "}
             </p>
 
-            <div className=""> <Link
-            href="mailto:priyansxu@proton.me"
-                  target="_blank"
-                  rel="noreferrer"
-                  className="pb-1 dark:text-zinc-800 text-zinc-100"
-                >
-             <Button size="xl" className="w-full font-bold" variant="brand">
-
+            <div>
+              <Link href="mailto:priyansxu@proton.me" target="_blank" rel="noreferrer">
+                <Button size="xl" className="w-full font-bold">
                   E-mail me
-              </Button>
-                </Link>
+                </Button>
+              </Link>
             </div>
           </div>
         </Wrapper>
